3fa12b925a2a59f4ca14525b0c02f266706d9b33
sayemaa <sayema990@gmail.com>
2022-05-24 14:27:59 +0600

    checkout page updated

--- src/Pages/Checkout/Checkout/Checkout.js
+++ src/Pages/Checkout/Checkout/Checkout.js
@@ -4,7 +4,13 @@ import './Checkout.css';
 const Checkout = () => {
     return (
         <div>
-            <h2 className='checkout'>Please Checkout</h2>
+            <h2 className='checkout my-5'>Billing Address</h2>
+            <form className='d-grid justify-items-center w-25 mx-auto billing-form'>
+                <input type="text" name="" placeholder='Full Name' required />
+                <input type="number" name="phone" placeholder='Phone no' required />
+                <input type="number" name="address" placeholder='Address' required />
+                <input className='submit-btn border-0 mx-auto text-center' type="submit" value="Submit" />
+            </form>
         </div>
     );
 };
